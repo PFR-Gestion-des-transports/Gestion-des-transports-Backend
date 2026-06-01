8ffe71468374f10703653de57738423fdb6ea333
Bailly-titouan <titouanbailly@outlook.fr>
2026-06-01 14:10:41 +0200

    Rajout de mot de passe dans la table utilisateur

--- MigrationSQL/Rajout-table-Utilisateur.sql
+++ MigrationSQL/Rajout-table-Utilisateur.sql
@@ -1,8 +1,9 @@
 CREATE TABLE Utilisateur (
-    Id       INT          NOT NULL AUTO_INCREMENT,
-    Prenom   VARCHAR(50)  NOT NULL,
-    Nom      VARCHAR(50)  NOT NULL,
-    Email    VARCHAR(255) NOT NULL UNIQUE,
+    Id          INT          NOT NULL AUTO_INCREMENT,
+    Prenom      VARCHAR(50)  NOT NULL,
+    Nom         VARCHAR(50)  NOT NULL,
+    Email       VARCHAR(255) NOT NULL UNIQUE,
+    MotDePasse  CHAR(64)     NOT NULL,
 
     CONSTRAINT PK_Utilisateur PRIMARY KEY (Id)
 );
