7c3e7c5f34db7aaa6805628908a0e5225704b9f2
Bailly-titouan <titouanbailly@outlook.fr>
2026-06-01 12:10:33 +0200

    KAN-5 : push des fichiers vide + pom.xml + .gitignore + application.properties

---
+++ src/main/java/gestiontransports/GestionTransportsApplication.java
@@ -0,0 +1,12 @@
+package gestiontransports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTransportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionTransportsApplication.class, args);
+    }
+}
---
+++ src/test/java/gestiontransports/GestionTransportsApplicationTests.java
@@ -0,0 +1,12 @@
+package gestiontransports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTransportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
