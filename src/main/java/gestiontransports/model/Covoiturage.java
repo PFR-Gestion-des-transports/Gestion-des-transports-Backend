4d71de1ffcbe4eb5494373910272f6f2f2eae4ed
Bailly-titouan <titouanbailly@outlook.fr>
2026-06-03 09:51:42 +0200

    Removed useless import

--- src/main/java/gestiontransports/model/Covoiturage.java
+++ src/main/java/gestiontransports/model/Covoiturage.java
@@ -11,7 +11,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.CascadeType;
 import java.time.LocalDateTime;
 
